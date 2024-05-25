70c953ed4b10ed9338e1019e8ff0a661cd17cc79
T1xelek <142146411+T1xelek@users.noreply.github.com>
2024-05-25 20:54:45 +0300

    Delete NeverHook 1.4 (1.12.2) directory

--- NeverHook 1.4 (1.12.2)/org/neverhook/client/event/events/Event.java
+++
@@ -1,9 +0,0 @@
-package org.neverhook.client.event.events;
-
-public interface Event {}
-
-
-/* Location:              C:\Users\Admin\OneDrive\Рабочий стол\NeverHook Crack.jar!\org\neverhook\client\event\events\Event.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
